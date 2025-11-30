487a29d9331a40c4b99d4227bb5dd293e651f04d
Yurii Panasuik <thoryur@gmail.com>
2025-11-30 03:37:59 +0200

    painterResource

--- app/src/main/java/com/prot/mytestapplication/MainActivity.kt
+++ app/src/main/java/com/prot/mytestapplication/MainActivity.kt
@@ -4,19 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.prot.mytestapplication.ui.theme.MyTestApplicationTheme
@@ -54,20 +61,50 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             Card(
                 modifier = Modifier
                     .fillMaxSize(0.9f),
-                //  .width(200.dp)
-                //  .height(390.dp),
                 shape = RoundedCornerShape(corner = CornerSize(15.dp)),
                 colors = CardDefaults.cardColors(
                     containerColor = Color.LightGray
                 ),
                 elevation = CardDefaults.cardElevation(
-                    defaultElevation = 4.dp
+                    defaultElevation = 3.dp
                 )
             ) {
 
             }
+
+            Card(
+                modifier = modifier
+                    .size(150.dp)
+                    .padding(5.dp),
+                shape = CircleShape,
+                border = BorderStroke(
+                    width = 0.5.dp,
+                    color = Color.LightGray,
+                ),
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 4.dp
+                ),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant.copy(
+                        alpha = 0.5f
+                    )
+                )
+            ) {
+                Image(
+                    painter = painterResource(
+                        id = R.drawable.johvi
+                    ),
+                    contentDescription = "Profile Image",
+                    modifier = Modifier.size(138.dp),
+                    contentScale = ContentScale.Crop
+                )
+
+
+            }
         }
     }
+
+
 }
 
 @Preview(
