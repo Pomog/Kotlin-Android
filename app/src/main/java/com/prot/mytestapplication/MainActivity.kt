11e0f509c13fca342a93c72e2848397a6239d611
Yurii Panasuik <thoryur@gmail.com>
2025-12-01 23:58:59 +0200

    CreatePersonalInfoCard

--- app/src/main/java/com/prot/mytestapplication/MainActivity.kt
+++ app/src/main/java/com/prot/mytestapplication/MainActivity.kt
@@ -6,8 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -15,9 +19,11 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -69,42 +75,48 @@ fun ScreenContent(modifier: Modifier = Modifier) {
                     defaultElevation = 3.dp
                 )
             ) {
-
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(5.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CreateImage()
+                    HorizontalDivider(thickness = 5.dp, color = Color.White)
+                    CreatePersonalInfoCard(Modifier.padding(5.dp))
+                }
             }
 
-            Card(
-                modifier = modifier
-                    .size(150.dp)
-                    .padding(5.dp),
-                shape = CircleShape,
-                border = BorderStroke(
-                    width = 0.5.dp,
-                    color = Color.LightGray,
-                ),
-                elevation = CardDefaults.cardElevation(
-                    defaultElevation = 4.dp
-                ),
-                colors = CardDefaults.cardColors(
-                    containerColor = MaterialTheme.colorScheme.surfaceVariant.copy(
-                        alpha = 0.5f
-                    )
-                )
-            ) {
-                Image(
-                    painter = painterResource(
-                        id = R.drawable.johvi
-                    ),
-                    contentDescription = "Profile Image",
-                    modifier = Modifier.size(138.dp),
-                    contentScale = ContentScale.Crop
-                )
-
-
-            }
         }
     }
+}
 
-
+@Composable
+fun CreatePersonalInfoCard(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Johvi",
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "Android Jetpack Compose",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.displayMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "The Comprehensive Bootcamp",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.displaySmall,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
 }
 
 @Preview(
@@ -115,5 +127,44 @@ fun ScreenContent(modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     MyTestApplicationTheme {
         ScreenContent()
+    }
+}
+
+@Composable
+fun CreateImage(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Card(
+            modifier = Modifier
+                .size(150.dp)
+                .padding(5.dp),
+            shape = CircleShape,
+            border = BorderStroke(
+                width = 0.5.dp,
+                color = Color.LightGray,
+            ),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 4.dp
+            ),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surfaceVariant.copy(
+                    alpha = 0.5f
+                )
+            )
+        ) {
+            Image(
+                painter = painterResource(
+                    id = R.drawable.johvi
+                ),
+                contentDescription = "Profile Image",
+                modifier = Modifier.size(138.dp),
+                contentScale = ContentScale.Crop
+            )
+        }
     }
 }
