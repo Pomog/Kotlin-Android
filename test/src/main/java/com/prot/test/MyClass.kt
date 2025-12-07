72aedbf1557149c31c9d80c02e5af591bd481788
Yurii Panasuik <thoryur@gmail.com>
2025-12-07 19:42:05 +0200

    HttpURLConnection

--- test/src/main/java/com/prot/test/MyClass.kt
+++ test/src/main/java/com/prot/test/MyClass.kt
@@ -1,8 +1,28 @@
 package com.prot.test
 
+import java.net.HttpURLConnection
+import java.net.URI
+
 fun main() {
     var name: String = "Yurii" // mutable
     name += " OP"
 
-    println("Hello, world! $name")
+    val b1: Byte = 256.toByte()
+    val b2: Byte = 128.toByte()
+    val b3: Byte = 255.toByte()
+
+
+    println("Hello, world! \n $b1 \n$b2 \n$b3")
+
+    simpleGet()
+}
+
+fun simpleGet() {
+    val url = URI("https://github.com/Pomog").toURL()
+    val conn = (url.openConnection() as HttpURLConnection)
+    conn.requestMethod = "GET"
+
+    conn.getInputStream()
+        .use { it.reader().readText() }
+        .also { println(it) }
 }
