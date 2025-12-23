502f301ec35ed5fe389769f5ab229f5657ee2655
Yurii Panasuik <thoryur@gmail.com>
2025-12-23 23:16:20 +0200

    CreateCircle

--- app/myapplication/src/main/java/com/prot/myapplication/IntroActivity.kt
+++ app/myapplication/src/main/java/com/prot/myapplication/IntroActivity.kt
@@ -1,16 +1,26 @@
 package com.prot.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.prot.myapplication.ui.theme.MyTestApplicationTheme
 
 class IntroActivity : ComponentActivity() {
@@ -19,29 +29,48 @@ class IntroActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyTestApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp()
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxWidth(),
+        color = Color(0xFFF5F5DC)
+    ) {
+        Text(text = "Hello Yurii")
+        CreateCircle()
+    }
+}
+
+@Preview
+@Composable
+fun CreateCircle() {
+    Card(
+        modifier = Modifier
+            .padding(3.dp)
+            .size(50.dp)
+            .clickable {
+                Log.d("Tap", "Circle: Tap")
+            },
+        shape = CircleShape
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(text = "Tap", modifier = Modifier.padding(3.dp))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyTestApplicationTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
