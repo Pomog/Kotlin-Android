2028e3c04329b96a4756b685423be1fb4ff7208b
Yurii Panasuik <thoryur@gmail.com>
2026-01-06 22:42:14 +0200

    Input Fields - Creating

---
+++ tipAppDemo/src/main/java/com/prot/tipappdemo/MainActivity.kt
@@ -0,0 +1,116 @@
+package com.prot.tipappdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.prot.tipappdemo.ui.theme.MyTestApplicationTheme
+import java.util.Locale
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApp {
+                TopHeader()
+            }
+
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    MyTestApplicationTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                content()
+
+            }
+
+        }
+    }
+}
+
+@Composable
+fun TopHeader(totalPerPerson: Double = 0.0) {
+    Surface( // TODO: change color
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+        color = Color(0xFF95C5FC)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(Locale.US, totalPerPerson)
+            Text(text = "Hello World", style = MaterialTheme.typography.headlineMedium)
+            Text(
+                text = total,
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.Bold,
+            )
+        }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Surface(
+        modifier = Modifier
+            .padding(5.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(10.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    ) {
+        Column {
+
+
+        }
+    }
+
+
+}
+
+
+@Preview
+@Composable
+fun GreetingPreview() {
+    MyTestApplicationTheme {
+        MyApp {
+            TopHeader()
+            MainContent()
+        }
+    }
+}
